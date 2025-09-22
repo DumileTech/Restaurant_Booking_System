7f00eda24fdc3287dfdcf777de3495fff1490c17
DumileTech <dumile@hapogroup.co.za>
2025-09-22 17:26:46 +0200

    Updated package-lock.json

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  {
-        protocol: 'https',
-        hostname: 'jaylcuvsaucgnmhmwynr.supabase.co', // Your Supabase project hostname
-        port: '',
-        pathname: '/**', // Allow all images from public storage buckets
-      },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
